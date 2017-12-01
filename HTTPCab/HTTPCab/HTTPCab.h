50afc0cd9b64d7ebd691e7c93e187f3005a59f14
Aleksey Zgurskiy <a.zgurskiy@nullgr.com>
2017-12-01 16:02:40 +0200

    Initial commit

---
+++ HTTPCab/HTTPCab/HTTPCab.h
@@ -0,0 +1,13 @@
+//
+//  HTTPCab.h
+//  HTTPCab
+//
+//  Created by Aleksey Zgurskiy on 01.12.2017.
+//  Copyright © 2017 Graviti Mobail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double HTTPCabVersionNumber;
+FOUNDATION_EXPORT const unsigned char HTTPCabVersionString[];
